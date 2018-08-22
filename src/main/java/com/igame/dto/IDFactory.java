6ecedf75bb5e62adef9aebbee5f226e3b38f933e
zhuanghaohui <464199843@qq.com>
2018-08-22 22:38:28 +0800

    代码优化 IDFactory

--- src/main/java/com/igame/dto/IDFactory.java
+++ src/main/java/com/igame/dto/IDFactory.java
@@ -16,7 +16,7 @@ public class IDFactory extends AbsDao {
 	
 	public static final String PL = "PL";//角色ID
 	
-	public static final String MA = "MA";//怪物
+	static final String MA = "MA";//怪物
 	
 
 
@@ -27,46 +27,59 @@ public class IDFactory extends AbsDao {
 	
 	 
 	public  long getNewIdByType(String type,int serverId){
-		synchronized(DBSeqcLock.getLock(serverId -1)){
-			IDSeq id = getDatastore(serverId).find(IDSeq.class, "type", PL).get();
-			if(id == null){
-				id = new IDSeq(PL,1);
+		return getNewId(serverId, type);
+
+	}
+
+	private long getNewId(int serverId, String type) {
+		synchronized (DBSeqcLock.getLock(serverId - 1)) {
+			IDSeq id = getDatastore(serverId).find(IDSeq.class, "type", type).get();
+			if (id == null) {
+				id = new IDSeq(type, 1);
 				getDatastore(serverId).save(id);
-				return Long.valueOf(String.valueOf(10000+serverId)+id.getValue());
-			}else{
+				return Long.valueOf(String.valueOf(10000 + serverId) + id.getValue());
+			} else {
 				getDatastore(serverId).update(id, getDatastore(serverId).createUpdateOperations(IDSeq.class).inc("value"));
 //				id = getDatastore().find(IDSeq.class, "type", AT).get();
-				return Long.valueOf(String.valueOf(10000+serverId)+(id.getValue()+1));
+				return Long.valueOf(String.valueOf(10000 + serverId) + (id.getValue() + 1));
 			}
 		}
-    	
 	}
-	
+
 	public long getNewIdMonster(int serverId){
-		synchronized(DBSeqcLock.getLock(serverId -1)){
-			IDSeq id = getDatastore(serverId).find(IDSeq.class, "type", MA).get();
-			if(id == null){
-				id = new IDSeq(MA,1);
-				getDatastore(serverId).save(id);
-				return Long.valueOf(String.valueOf(10000+serverId)+id.getValue());
-			}else{
-				getDatastore(serverId).update(id, getDatastore(serverId).createUpdateOperations(IDSeq.class).inc("value"));
-	//			id = getDatastore().find(IDSeq.class, "type", AT).get();
-				return Long.valueOf(String.valueOf(10000+serverId)+(id.getValue()+1));
-			}
-		}
-		
-	    	
+		return getNewId(serverId, MA);
+
+
 	}
 
     private static final IDFactory domain = new IDFactory();
-    public static final IDFactory ins(){return domain;}
+    public static IDFactory ins(){return domain;}
    
     
     private IDFactory(){}
 
-    
-    
-    
 
+	/**
+	 *
+	 * @author Marcus.Z
+	 *
+	 */
+	public static class DBSeqcLock {
+		private static final int DEFAULT_SIZE = 100;
+		private static Object[] locks = new Object[100];
+		private static Object oo = new Object();
+
+
+		static Object getLock(int index) {
+			if (index < 0 || index >= locks.length) {
+				return oo;
+			}
+			return locks[index];
+		}
+
+		static {
+			for (int i = 0; i < DEFAULT_SIZE; i++)
+				locks[i] = new Object();
+		}
+	}
 }
