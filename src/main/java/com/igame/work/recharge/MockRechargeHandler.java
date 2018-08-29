e6ecdc613582fdbf4e0ac8f4b217fa6649a6e17f
zhuanghaohui <464199843@qq.com>
2018-08-29 14:28:10 +0800

    终极馈赠活动

--- src/main/java/com/igame/work/recharge/MockRechargeHandler.java
+++ src/main/java/com/igame/work/recharge/MockRechargeHandler.java
@@ -1,9 +1,13 @@
 package com.igame.work.recharge;
 
+import com.igame.core.event.EventType;
+import com.igame.core.event.PlayerEventObserver;
 import com.igame.core.handler.ReconnectedHandler;
 import com.igame.core.handler.RetVO;
 import com.igame.work.MProtrol;
 import com.igame.work.PlayerEvents;
+import com.igame.work.activity.ActivityConfigTemplate;
+import com.igame.work.activity.ActivityDataManager;
 import com.igame.work.gm.service.GMService;
 import com.igame.work.user.dto.Player;
 import com.smartfoxserver.v2.entities.data.ISFSObject;
@@ -34,5 +38,28 @@ public class MockRechargeHandler extends ReconnectedHandler {
         }
 
         return new RetVO();
+    }
+
+    @Override
+    protected PlayerEventObserver observeEvent() {
+        return new PlayerEventObserver() {
+            @Override
+            public EventType interestedType() {
+                return PlayerEvents.RECHARGE;
+            }
+
+            @Override
+            public void observe(Player player, Object event) {
+                // TODO 这块是终极馈赠的活动 是新建个活动类移到活动模块还是放在这里
+                ActivityConfigTemplate template = ActivityDataManager.activityConfig.getTemplate(1003);
+                if (template == null) {
+                    return;
+                }
+                int rechargeLimit = Integer.parseInt(template.getGet_value());
+                if ((int) event > rechargeLimit) {
+                    GMService.processGM(player, template.getActivity_drop());
+                }
+            }
+        };
     }
 }
