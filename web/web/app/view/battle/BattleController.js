13c9d5e0cc7c6b9a9d9fa891f1ae4495b6632153
zhuanghaohui <464199843@qq.com>
2018-09-27 18:24:53 +0800

    extjs battle emulator

--- web/web/app/view/battle/BattleController.js
+++ web/web/app/view/battle/BattleController.js
@@ -11,6 +11,7 @@ Ext.define('web.view.battle.BattleController', {
     init: function() {
         self = this;
         self.v = 5;
+        self.v2 = -5;
         createjs.Ticker.timingMode = createjs.Ticker.RAF;
         createjs.Ticker.addEventListener("tick", this.tick);
     }
@@ -18,10 +19,17 @@ Ext.define('web.view.battle.BattleController', {
         self.stage = new createjs.Stage("demoCanvas");
 
         self.circle = new createjs.Shape();
-        self.circle.graphics.beginFill("red").drawCircle(0, 0, 50);
+        self.circle.graphics.beginFill("blue").drawCircle(0, 0, 50);
         self.circle.x = 100;
         self.circle.y = 100;
         self.stage.addChild(self.circle);
+
+        self.circle2 = new createjs.Shape();
+        self.circle2.graphics.beginFill("red").drawCircle(0, 0, 50);
+        self.circle2.x = 500;
+        self.circle2.y = 100;
+        self.stage.addChild(self.circle2);
+
         self.stage.update(event);
     }
     ,tick: function(event) {
@@ -29,6 +37,11 @@ Ext.define('web.view.battle.BattleController', {
         self.circle.x+=self.v;
         if (self.circle.x>=500 || self.circle.x<=0)
             self.v = -self.v;
+
+        self.circle2.x+=self.v2;
+        if (self.circle2.x>=500 || self.circle2.x<=0)
+            self.v2 = -self.v2;
+
         self.stage.update(event);
     }
 });
