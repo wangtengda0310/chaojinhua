959c8545cbc4f7a4a0786a1afbb896d455bfa64e
zhuanghaohui <464199843@qq.com>
2018-09-28 17:05:13 +0800

    空指针

--- game/src/main/java/com/igame/work/activity/ActivityDto.java
+++ game/src/main/java/com/igame/work/activity/ActivityDto.java
@@ -4,11 +4,12 @@ import com.igame.core.db.BasicDto;
 import org.mongodb.morphia.annotations.Entity;
 
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Entity(noClassnameStored = true)
 public class ActivityDto extends BasicDto {
     private int activityId; // activityId 在外层 方便删除不存在活动的数据
-    protected Map<Long, ActivityOrderDto> orderData;   // key playerId
+    protected Map<Long, ActivityOrderDto> orderData = new ConcurrentHashMap<>();   // key playerId
 
     public int getActivityId() {
         return activityId;
